b7883b49c36d40901f195dd6f45945a083f15143
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-03-04 00:15:04 +0530

    dash bord nave bar make

--- src/Layout/DashBordLayout.js
+++ src/Layout/DashBordLayout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Shared/Header";
-import DashBord from "../Page/DashBord/DashBord";
 import DashNave from "../Page/DashBord/DashNave/DashNave";
 
 const DashBordLayout = () => {
@@ -10,13 +9,12 @@ const DashBordLayout = () => {
       <div className="sticky top-0 z-50">
         <Header></Header>
       </div>
-      <div className="flex">
-        <div>
-          <DashNave />
-        </div>
-        <div>
-          <Outlet />
-        </div>
+      <div className="sticky top-0 z-50">
+        <DashNave />
+      </div>
+
+      <div>
+        <Outlet />
       </div>
     </div>
   );
--- src/Page/DashBord/DashNave/DashNave.js
+++ src/Page/DashBord/DashNave/DashNave.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import {  NavLink } from "react-router-dom";
 
 const DashNave = () => {
-    return (
-        <div>
-            nave nar
-        </div>
-    );
+  const manuList = (
+    <>
+      <li>
+        <NavLink to="/home">Home</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/Products">Products</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/DashBord">DashBord</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/Register">Register</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar  p-4  bg-blue-200 rounded-none  shadow-lg ">
+      <div className="navbar-start  flex">
+        <ul className="menu menu-horizontal p-0 text-xl font-semibold text-blue-800">
+          {manuList}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DashNave;
