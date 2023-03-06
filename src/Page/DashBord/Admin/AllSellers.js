7248323902f5d223b469fd969d2370916662e96d
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-03-07 00:59:20 +0530

    admin done

--- src/Page/DashBord/Admin/AllSellers.js
+++ src/Page/DashBord/Admin/AllSellers.js
@@ -32,73 +32,93 @@ const AllSellers = () => {
         });
     }
 }
-  return (
+
+const hadelVerify = (id) => {
+  fetch(`http://localhost:8000/update/${id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            refetch();
+            return toast.success("verify Success Full !!");
+          }
+        });
+};
+return (
+  <div>
     <div>
-      <div>
-        <div></div>
-        {data?.length === 0 ? (
-          <h1 className="text-black text-center my-8 text-2xl">
-            No Data Plies{" "}
-            <Link className="text-red-800 underline" to="/">
-              Add book..
-            </Link>
-          </h1>
-        ) : (
-          <div className=" w-full">
-            <div className="flex items-center justify-center ">
-              <h2 className="text-center  text-3xl md:text-4xl  font-bold inline-block text-blue-900 my-4   border-b-2    pb-3 uppercase ">
-                All Sellers
-              </h2>
-            </div>
-            <div className="mt-5 ">
-              <div className="overflow-x-auto">
-                <table className="table table-compact table-zebra  w-full">
-                  <thead className="text-2xl font-semibold text-primary bg-accent text-center ">
-                    <tr>
-                      <th>S.I.</th>
-                      <th>Name </th>
-                      <th>Email</th>
-                      <th>Type</th>
-                      <th>Verify</th>
-                      <th>Id</th>
+      <div></div>
+      {data?.length === 0 ? (
+        <h1 className="text-black text-center my-8 text-2xl">
+          No Data Plies{" "}
+          <Link className="text-red-800 underline" to="/">
+            Add book..
+          </Link>
+        </h1>
+      ) : (
+        <div className=" w-full">
+          <div className="flex items-center justify-center ">
+            <h2 className="text-center  text-3xl md:text-4xl  font-bold inline-block text-blue-900 my-4   border-b-2    pb-3 uppercase ">
+              All Sellers
+            </h2>
+          </div>
+          <div className="mt-5 ">
+            <div className="overflow-x-auto">
+              <table className="table table-compact table-zebra  w-full">
+                <thead className="text-2xl font-semibold text-primary bg-accent text-center ">
+                  <tr>
+                    <th>S.I.</th>
+                    <th>Name </th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Verify</th>
+                    <th>Id</th>
 
-                      <th>Action</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody className="text-black font-semibold text-center ">
+                  {data.map((product, idx) => (
+                    <tr key={product._id}>
+                      <th>{idx + 1}</th>
+                      <td>{product.name}</td>
+                      <td>{product.email}</td>
+                      <td>{product.type}</td>
+                      <td>{product._id}</td>
+                      <td>
+                        {product?.verify === "verify" ? <h1 className="font-bold">verifyed</h1> :  <button
+                          onClick={()=>{hadelVerify(product._id)}}
+                          className="btn btn-success btn-xs"
+                        >
+                          verify
+                        </button>}
+                       
+                      </td>
+
+                      <td>
+                        <button
+                          onClick={() => {
+                            handelDelete(product._id);
+                          }}
+                        >
+                          <AiFillCloseCircle className="text-center text-2xl font-bold text-red-500  "></AiFillCloseCircle>
+                        </button>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody className="text-black font-semibold text-center ">
-                    {data.map((product, idx) => (
-                      <tr key={product._id}>
-                        <th>{idx + 1}</th>
-                        <td>{product.name}</td>
-                        <td>{product.email}</td>
-                        <td>{product.type}</td>
-                        <td>{product._id}</td>
-                        <td>
-                          <button className="btn btn-success btn-xs">
-                            verify
-                          </button>
-                        </td>
-
-                        <td>
-                          <button
-                            onClick={() => {
-                              handelDelete(product._id);
-                            }}
-                          >
-                            <AiFillCloseCircle className="text-center text-2xl font-bold text-red-500  "></AiFillCloseCircle>
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
-  );
+  </div>
+);
 };
 
 export default AllSellers;
