ba9f15121ca4c06945d81e8484f808705eb8f282
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-03-07 16:29:46 +0530

    stripr payment sistem

---
+++ src/Page/DashBord/Buyer/Modal.js
@@ -0,0 +1,136 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
+const Modal = ({ modal, refetch, setModal }) => {
+  const { price, _id } = modal;
+  const [errord, setErrord] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+  // fetch
+  const [clientSecret, setClientSecret] = useState("");
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:8000/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ price }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [price]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setErrord(error.message);
+    } else {
+      setErrord("");
+    }
+
+    const { paymentIntent, error: errors } = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: "_id",
+          },
+        },
+      }
+    );
+
+    if (errors) {
+      setErrord(error?.message);
+    }
+    console.log("paymentIntent", paymentIntent);
+
+    if (paymentIntent?.status) {
+      fetch(`http://localhost:8000/booking/${_id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.modifiedCount) {
+            refetch();
+            setModal("");
+            toast.success("success full payment !!");
+          }
+        });
+    }
+  };
+  return (
+
+
+
+<div >
+<input type="checkbox" id="my-modal-3" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box relative">
+    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+   
+    <form onSubmit={handleSubmit}>
+          <CardElement
+            className="border-2 mt-5 p-3 rounded-sm"
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#dfe5eb",
+                  "::placeholder": {
+                    color: "#dfe5eb",
+                  },
+                },
+                invalid: {
+                  color: "#dfe5eb",
+                },
+              },
+            }}
+          />
+          <button
+            className="w-full mt-3 text-center btn btn-primary"
+            type="submit"
+            disabled={!stripe}
+          >
+            Pay
+          </button>
+          <p className="text-red-600">{errord}</p>
+        </form>
+
+
+  </div>
+</div>
+</div>
+
+// <div className="relative ">
+
+
+//   <div className="w-[400px] absolute top-0 left-0 right-0">
+    
+//   </div>
+// </div>
+
+
+  );
+};
+
+export default Modal;
--- src/Page/DashBord/Buyer/MyBook.js
+++ src/Page/DashBord/Buyer/MyBook.js
@@ -1,14 +1,30 @@
+import { CardElement, Elements, useElements, useStripe } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { toast } from "react-hot-toast";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Components/Context/UserContext";
+import Modal from "./Modal";
+
+const stripePromise = loadStripe(
+  "pk_test_51MirOnSClmyxetKpgfUtBMbu1B7rBd8boOZgUoXN9uKSD9uSXD2JxeDaNfXZC8fCPn1p3rAchrcvjGbtnLDVgJik00Qf7IFgt1"
+);
 
 const MyBook = () => {
   const { user } = useContext(AuthContext);
+  const [modal, setModal] = useState(null);
+  const [errord, setErrord] = useState("");
+  
+  
+
   const email = user?.email;
-  const { data = [] , refetch , isLoading} = useQuery({
+  const {
+    data = [],
+    refetch,
+    isLoading,
+  } = useQuery({
     queryKey: ["email"],
     queryFn: async () => {
       const res = await fetch(`http://localhost:8000/booking?email=${email}`);
@@ -16,14 +32,13 @@ const MyBook = () => {
       return data;
     },
   });
-  if(isLoading){
+  if (isLoading) {
     return (
       <div className="w-16 h-16 mx-auto my-auto border-4 border-dashed rounded-full animate-spin border-violet-400"></div>
-    )
+    );
   }
 
   const handelDelete = (id) => {
-
     const conformation = window.confirm("Are you sore !!");
     if (conformation) {
       fetch(`http://localhost:8000/Order/${id}`, {
@@ -42,76 +57,106 @@ const MyBook = () => {
     }
   };
 
+  const handelPay = (product) => {
+    setModal(product);
+    console.log("product", product);
+  };
+
+
+  // mayment
+
+
   return (
     <div>
       <div></div>
-      {data?.length === 0 ? (
-        <h1 className="text-black text-center my-8 text-2xl">
-          No Data Plies{" "}
-          <Link className="text-red-800 underline" to="/">
-            Add book..
-          </Link>
-        </h1>
-      ) : (
-        <div className=" w-full">
-          <div className="flex items-center justify-center ">
-            <h2 className="text-center  text-3xl md:text-4xl  font-bold inline-block text-blue-900 my-4   border-b-2    pb-3 uppercase ">
-              My products
-            </h2>
-          </div>
-          <div className="mt-5 ">
-            <div className="overflow-x-auto">
-              <table className="table table-compact table-zebra  w-full">
-                <thead className="text-2xl font-semibold text-primary bg-accent text-center ">
-                  <tr>
-                    <th>S.I.</th>
-                    <th>Image </th>
-                    <th>product</th>
-                    <th>phone</th>
-                    <th>Location</th>
-                    <th>price</th>
-                    <th>Pay</th>
+      <div>
+        {data?.length === 0 ? (
+          <h1 className="text-black text-center my-8 text-2xl">
+            No Data Plies{" "}
+            <Link className="text-red-800 underline" to="/">
+              Add book..
+            </Link>
+          </h1>
+        ) : (
+          <div className=" w-full">
+            <div className="flex items-center justify-center ">
+              <h2 className="text-center  text-3xl md:text-4xl  font-bold inline-block text-blue-900 my-4   border-b-2    pb-3 uppercase ">
+                My products
+              </h2>
+            </div>
+            <div className="mt-5 ">
+              <div className="overflow-x-auto">
+                <table className="table table-compact table-zebra  w-full">
+                  <thead className="text-2xl font-semibold text-primary bg-accent text-center ">
+                    <tr>
+                      <th>S.I.</th>
+                      <th>Image </th>
+                      <th>product</th>
+                      <th>phone</th>
+                      <th>Location</th>
+                      <th>price</th>
+                      <th>Pay</th>
 
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody className="text-black font-semibold text-center ">
-                  {data.map((product, idx) => (
-                    <tr key={product._id}>
-                      <th>{idx + 1}</th>
-                      <td>
-                        <div className="avatar">
-                          <div className="w-12 rounded">
-                            <img
-                              src={product?.img}
-                              alt="Tailwind-CSS-Avatar-component"
-                            />
-                          </div>
-                        </div>
-                      </td>
-                      <td>{product.productName}</td>
-                      <td>{product.phone}</td>
-                      <td>{product.location}</td>
-                      <td>{product.price} RS</td>
-                      <td><button className="btn btn-sm btn-success">pay</button></td>
-
-                      <td>
-                        <button
-                          onClick={() => {
-                            handelDelete(product._id);
-                          }}
-                        >
-                          <AiFillCloseCircle className="text-center text-2xl font-bold text-red-500  "></AiFillCloseCircle>
-                        </button>
-                      </td>
+                      <th>Action</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="text-black font-semibold text-center ">
+                    {data.map((product, idx) => (
+                      <tr key={product._id}>
+                        <th>{idx + 1}</th>
+                        <td>
+                          <div className="avatar">
+                            <div className="w-12 rounded">
+                              <img
+                                src={product?.img}
+                                alt="Tailwind-CSS-Avatar-component"
+                              />
+                            </div>
+                          </div>
+                        </td>
+                        <td>{product.productName}</td>
+                        <td>{product.phone}</td>
+                        <td>{product.location}</td>
+                        <td>{product.price} RS</td>
+                        <td>
+                          <label htmlFor="my-modal-3">
+
+                          <button
+                            onClick={() => {
+                              handelPay(product);
+                            }}
+                            className="btn btn-sm btn-success"
+                          >
+                            pay
+                          </button>
+                          </label>
+                        </td>
+
+                        <td>
+                          <button
+                            onClick={() => {
+                              handelDelete(product._id);
+                            }}
+                          >
+                            <AiFillCloseCircle className="text-center text-2xl font-bold text-red-500  "></AiFillCloseCircle>
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+      {/* modal={modal}  setModal ={setModal} refetch ={refetch} */}
+
+      {modal && (
+        <Elements stripe={stripePromise}>
+          <Modal modal={modal} setModal ={setModal} refetch ={refetch}></Modal>
+        </Elements>
+       )} 
     </div>
   );
 };
